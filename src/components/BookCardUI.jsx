689db7f401a358c4722b4f7c3cdf6b4a0e98581a
Wendy <111409717+wendy-aik-maung@users.noreply.github.com>
2022-12-10 23:09:11 +0630

    create checkout page

--- src/components/BookCardUI.jsx
+++ src/components/BookCardUI.jsx
@@ -9,110 +9,116 @@ import { AiOutlineStar } from "react-icons/ai";
 import { NavLink, useLocation } from "react-router-dom";
 
 const variants = {
-  hover: {
-    x: 0,
-    scale: [1, 1.1, 1],
-    // rotateZ: [10, -10, 0],
-    opacity: [0.5, 1],
-  },
-  initial: {
-    x: 0,
-    scale: 1,
-    // rotateZ: [10, -10, 0],
-    opacity: [0.5, 1],
-  },
+	hover: {
+		x: 0,
+		scale: [1, 1.1, 1],
+		// rotateZ: [10, -10, 0],
+		opacity: [0.5, 1],
+	},
+	initial: {
+		x: 0,
+		scale: 1,
+		// rotateZ: [10, -10, 0],
+		opacity: [0.5, 1],
+	},
 };
 
-const BookCardUI = ({ book: { attributes, id }, handleClick }) => {
-  const location = useLocation();
+const BookCardUI = ({
+	book: { attributes, id },
+	book,
+	handleClick,
+	bookCount,
+	handleBookCount,
+	handleBookCart,
+}) => {
+	const location = useLocation();
 
-  const {
-    title,
-    price,
-    discount,
-    image: { data },
-    categories,
-  } = attributes;
-  const category = categories.data[0].attributes.name;
-  const url = `http://localhost:1337${data[0].attributes.url}`;
+	const {
+		title,
+		price,
+		discount,
+		image: { data },
+		categories,
+	} = attributes;
+	const category = categories.data[0].attributes.name;
+	const url = `http://localhost:1337${data[0].attributes.url}`;
 
-  const [isHovered, setIsHovered] = useState(false);
-  const bookHover = useRef(null);
-  const bookLeave = useRef(null);
-  const discountPrice = (price - (price * discount) / 100).toFixed(2);
-  const handleMouseEnter = () => {
-    bookHover.current.className = "hidden";
-    bookLeave.current.className = "block";
-    setIsHovered(true);
-  };
-  const handleMouseLeave = () => {
-    bookHover.current.className = "block";
-    bookLeave.current.className = "hidden";
-    setIsHovered(false);
-  };
+	const [isHovered, setIsHovered] = useState(false);
+	const bookHover = useRef(null);
+	const bookLeave = useRef(null);
+	const discountPrice = (price - (price * discount) / 100).toFixed(2);
+	const handleMouseEnter = () => {
+		bookHover.current.className = "hidden";
+		bookLeave.current.className = "block";
+		setIsHovered(true);
+	};
+	const handleMouseLeave = () => {
+		bookHover.current.className = "block";
+		bookLeave.current.className = "hidden";
+		setIsHovered(false);
+	};
 
-  return (
-    <div
-      className=" col-span-1 bg-background_color "
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    >
-      <motion.div
-        variants={variants}
-        animate={isHovered ? "hover" : "initial"}
-        className=" relative"
-      >
-        <img src={url} className=" w-full object-cover h-96" alt="" />
-        <div className=" bg-primary absolute top-0 left-0 p-2">
-          <span className=" text-sm">discount</span>{" "}
-          <span>{!discount ? 0 : discount}%</span>
-        </div>
-      </motion.div>
-      <motion.div
-        variants={variants}
-        animate={isHovered ? "hover" : "initial"}
-        ref={bookHover}
-      >
-        <p className=" text-center text-primary mt-3">{title}</p>
-        <div className=" flex justify-center gap-10 my-2">
-          <span className=" text-primary">${discountPrice}</span>
-          <span className=" text-text_color line-through">${price}</span>
-        </div>
-      </motion.div>
-      {/* While Hover */}
-      <motion.div
-        variants={variants}
-        animate={isHovered ? "hover" : "initial"}
-        ref={bookLeave}
-        className="hidden"
-      >
-        <div className=" flex justify-center gap-2 my-3">
-          <NavLink
-            to={location.pathname == "/" ? `shop/${category}/${id}` : `${id}`}
-          >
-            <button className=" btn btn-circle btn-secondary btn-sm btn-outline">
-              <BsEye className=" text-xl" />
-            </button>
-          </NavLink>
+	return (
+		<div
+			className=" col-span-1 bg-background_color "
+			onMouseEnter={handleMouseEnter}
+			onMouseLeave={handleMouseLeave}>
+			<motion.div
+				variants={variants}
+				animate={isHovered ? "hover" : "initial"}
+				className=" relative">
+				<img src={url} className=" w-full object-cover h-96" alt="" />
+				<div className=" bg-primary absolute top-0 left-0 p-2">
+					<span className=" text-sm">discount</span>{" "}
+					<span>{!discount ? 0 : discount}%</span>
+				</div>
+			</motion.div>
+			<motion.div
+				variants={variants}
+				animate={isHovered ? "hover" : "initial"}
+				ref={bookHover}>
+				<p className=" text-center text-primary mt-3">{title}</p>
+				<div className=" flex justify-center gap-10 my-2">
+					<span className=" text-primary">${discountPrice}</span>
+					<span className=" text-text_color line-through">${price}</span>
+				</div>
+			</motion.div>
+			{/* While Hover */}
+			<motion.div
+				variants={variants}
+				animate={isHovered ? "hover" : "initial"}
+				ref={bookLeave}
+				className="hidden">
+				<div className=" flex justify-center gap-2 my-3">
+					<NavLink
+						to={location.pathname == "/" ? `shop/${category}/${id}` : `${id}`}>
+						<button className=" btn btn-circle btn-secondary btn-sm btn-outline">
+							<BsEye className=" text-xl" />
+						</button>
+					</NavLink>
 
-          <button className=" btn btn-circle btn-secondary btn-sm btn-outline">
-            <BsBookmarkPlus className=" text-xl" />
-          </button>
+					<button className=" btn btn-circle btn-secondary btn-sm btn-outline">
+						<BsBookmarkPlus className=" text-xl" />
+					</button>
 
-          <button className=" btn btn-circle btn-secondary btn-sm btn-outline">
-            <RiShoppingBasketLine className=" text-xl" />
-          </button>
-        </div>
-        <div className=" flex justify-center gap-1 ">
-          <AiOutlineStar className=" text-primary " />
-          <AiOutlineStar className=" text-primary " />
-          <AiOutlineStar className=" text-primary " />
-          <AiOutlineStar className=" text-primary " />
-          <AiOutlineStar className=" text-primary " />
-        </div>
-      </motion.div>
-    </div>
-  );
+					<button
+						className=" btn btn-circle btn-secondary btn-sm btn-outline"
+						onClick={() => {
+							handleBookCount(), handleBookCart(book);
+						}}>
+						<RiShoppingBasketLine className=" text-xl" />
+					</button>
+				</div>
+				<div className=" flex justify-center gap-1 ">
+					<AiOutlineStar className=" text-primary " />
+					<AiOutlineStar className=" text-primary " />
+					<AiOutlineStar className=" text-primary " />
+					<AiOutlineStar className=" text-primary " />
+					<AiOutlineStar className=" text-primary " />
+				</div>
+			</motion.div>
+		</div>
+	);
 };
 
 export default BookCardUI;
--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,45 +1,49 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink, Outlet } from "react-router-dom";
+import { BookContext } from "../App";
 //service
 import GetCategories from "../services/GetCategories";
 
 const ShopPage = () => {
-  const { response, loading } = GetCategories();
-  if (loading) return null;
-  console.log(response);
-  return (
-    <div className=" bg-background_color h-screen pt-16 overflow-auto">
-      <div className=" h-full grid grid-cols-5">
-        <div className=" col-span-1 h-full bg-background_color border-r">
-          <h1 className=" text-center uppercase text-xl mt-5 font-bold text-text_color font-title">
-            categories
-          </h1>
-          <div className=" p-2">
-            {response.map((category) => (
-              <div
-                className=" border-b p-1 hover:bg-background_gray_color "
-                key={category.id}
-              >
-                <NavLink
-                  to={`${category.attributes.name}`}
-                  className={({ isActive }) =>
-                    isActive
-                      ? " text-primary block text-center"
-                      : " text-text_color block text-center"
-                  }
-                >
-                  {category.attributes.name}
-                </NavLink>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className=" col-span-4 h-full bg-background_color">
-          <Outlet />
-        </div>
-      </div>
-    </div>
-  );
+	const { handleBookCount, handleBookCart } = useContext(BookContext);
+
+	const { response, loading } = GetCategories();
+	if (loading) return null;
+	console.log(response);
+	return (
+		<div className=" bg-background_color h-screen pt-16 overflow-auto">
+			<div className=" h-full grid grid-cols-5">
+				<div className=" col-span-1 h-full bg-background_color border-r">
+					<h1 className=" text-center uppercase text-xl mt-5 font-bold text-text_color font-title">
+						categories
+					</h1>
+					<div className=" p-2">
+						{response.map((category) => (
+							<div
+								className=" border-b p-1 hover:bg-background_gray_color "
+								key={category.id}>
+								<NavLink
+									to={`${category.attributes.name}`}
+									className={({ isActive }) =>
+										isActive
+											? " text-primary block text-center"
+											: " text-text_color block text-center"
+									}>
+									{category.attributes.name}
+								</NavLink>
+							</div>
+						))}
+					</div>
+				</div>
+				<div className=" col-span-4 h-full bg-background_color">
+					<Outlet
+						handleBookCart={handleBookCart}
+						handleBookCount={handleBookCount}
+					/>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ShopPage;
