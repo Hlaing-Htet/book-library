4a3b83551ff2076f5768c306e49c5aaf0c6816e0
Hlaing-Htet <hlainehtet.hh.hh@gmail.com>
2022-12-15 18:08:31 +0630

    shoplist loading

--- src/components/ShopList.jsx
+++ src/components/ShopList.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Outlet, useParams } from "react-router-dom";
-
+import Loading from "../components/Loading";
 import BookCardUI from "../components/BookCardUI";
 //services
 import GetBooks from "../services/GetBooks";
 
 const ShopList = () => {
   const { response, loading } = GetBooks();
-  // if (loading) return null;
+  if (loading) return <Loading />;
 
   const { name } = useParams();
 
@@ -18,7 +18,7 @@ const ShopList = () => {
     );
 
   return (
-    <div >
+    <div>
       <Outlet />
 
       <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 mx-0 md:mx-5  p-1 sm:p-5">
